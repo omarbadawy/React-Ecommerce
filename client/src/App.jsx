e553eb39c8e585495d8093a9e78f8c822675387e
omarbadawy <omarbadawy10022@gmail.com>
2021-02-26 11:04:00 +0200

    Added React-Router And React-Router-BootStrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
     return (
@@ -10,7 +12,8 @@ const App = () => {
             <Header />
             <main className="py-3">
                 <Container>
-                    <HomeScreen />
+                    <Route path="/" exact component={HomeScreen} />
+                    <Route path="/product/:id" component={ProductScreen} />
                 </Container>
             </main>
             <Footer />
