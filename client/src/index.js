88c80cb83e9e682e17d01d73b08755f8303b438e
omarbadawy <omarbadawy10022@gmail.com>
2021-03-16 13:18:42 +0200

    Added basic product reducer setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import store from './store'
 import './bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 )
 
