6551f256d169209f57632e769e54ff1c028ed9e6
omarbadawy <omarbadawy10022@gmail.com>
2021-05-02 17:38:46 +0200

    Added Protect middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,10 @@
 const asyncHandler = require('express-async-handler')
+const generateToken = require('../utils/generateToken')
 const User = require('../models/userModel')
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
-// @access Public
+// @access  Public
 
 exports.authUser = asyncHandler(async (req, res, next) => {
     const { email, password } = req.body
@@ -16,10 +17,30 @@ exports.authUser = asyncHandler(async (req, res, next) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null,
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
         throw new Error('Invalid email or password')
+    }
+})
+
+// @desc    Auth user profile
+// @route   POST /api/users/profile
+// @access  Private
+
+exports.getUserProfile = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
     }
 })
