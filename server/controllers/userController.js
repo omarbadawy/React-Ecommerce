fea9518ac9e22d223391eadc6cf959ee018dc272
omarbadawy <omarbadawy10022@gmail.com>
2021-05-02 17:57:29 +0200

    Finished Auth in server side

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,6 +25,40 @@ exports.authUser = asyncHandler(async (req, res, next) => {
     }
 })
 
+// @desc    Register a new user
+// @route   POST /api/users/
+// @access  Public
+
+exports.registerUser = asyncHandler(async (req, res, next) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('user already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 // @desc    Auth user profile
 // @route   POST /api/users/profile
 // @access  Private
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { authUser, getUserProfile } = require('../controllers/userController')
+const { authUser, registerUser, getUserProfile } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 
 router.route('/profile').get(protect, getUserProfile)
