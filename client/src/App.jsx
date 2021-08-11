d3a54f026171af7bc6714270bd556d337e9eadc1
omarbadawy <omarbadawy10022@gmail.com>
2021-08-11 23:38:57 +0200

    User Login&Register finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import CartScreen from './screens/CartScreen'
 import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
@@ -13,9 +15,11 @@ const App = () => {
             <Header />
             <main className="py-3">
                 <Container>
-                    <Route path="/" exact component={HomeScreen} />
+                    <Route path="/login" component={LoginScreen} />
+                    <Route path="/register" component={RegisterScreen} />
                     <Route path="/product/:id" component={ProductScreen} />
                     <Route path="/cart/:id?" component={CartScreen} />
+                    <Route path="/" exact component={HomeScreen} />
                 </Container>
             </main>
             <Footer />
